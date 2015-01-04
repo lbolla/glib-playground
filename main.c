e72390d0a25c0603c0c64b61230723af912605f1
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2015-01-04 20:01:02 +0000

    Added note regarding casting function pointers to data pointers

--- main.c
+++ main.c
@@ -11,13 +11,17 @@ void usage(void)
 
 int main(int argn, char *argv[])
 {
-	typedef int (main_t)();
+	typedef int (*main_ptr)();
 	int retval;
+
 	GHashTable *main_map = g_hash_table_new(g_str_hash, g_str_equal);
-	g_hash_table_insert(main_map, "slist", (gpointer)main_slist);
-	g_hash_table_insert(main_map, "list", (gpointer)main_list);
-	g_hash_table_insert(main_map, "loop", (gpointer)main_main_loop);
-	g_hash_table_insert(main_map, "testing", (gpointer)main_testing);
+	// Note: ANSI prohibits casting function pointers to data
+	// pointers. We should instead store a pointer to the function
+	// pointer.
+	g_hash_table_insert(main_map, "slist", (gpointer)&main_slist);
+	g_hash_table_insert(main_map, "list", (gpointer)&main_list);
+	g_hash_table_insert(main_map, "loop", (gpointer)&main_main_loop);
+	g_hash_table_insert(main_map, "testing", (gpointer)&main_testing);
 
 	if (argn < 2) {
 		g_warning("No main given");
@@ -27,7 +31,7 @@ int main(int argn, char *argv[])
 	}
 
 	const char *what = g_strstrip(argv[1]);
-	main_t *main_p = g_hash_table_lookup(main_map, what);
+	main_ptr main_p = (main_ptr)g_hash_table_lookup(main_map, what);
 	if (main_p == NULL) {
 		g_warning("No main found for \"%s\".", what);
 		usage();
